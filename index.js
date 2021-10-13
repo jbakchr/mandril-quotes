8a6133938dddef2d54953131b6490647db59159a
Jonas Bak Phillipson <jbakchr@gmail.com>
2021-10-13 23:14:03 +0200

    Added initial programoversigter

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const chalk = require("chalk");
+
 const quotes = require("./lib/quotes");
 const warnings = require("./lib/warnings.json");
+const programOverviews = require("./lib/program-overviews.json");
 
 const getRandomIndex = (arr) => {
   return Math.floor(Math.random() * arr.length);
@@ -15,7 +18,17 @@ const getWarning = () => {
   console.log(`S${season}:E${episode}: ${warning}`);
 };
 
+const enProgramOversigt = () => {
+  const { season, episode, program_overview } =
+    programOverviews[getRandomIndex(programOverviews)];
+  console.log(chalk.underline.bold(`Programoversigt (S${season}:E${episode})`));
+  program_overview.forEach(({ time, title }) => {
+    console.log(`${time}  ${title}`);
+  });
+};
+
 module.exports = {
   spisIld,
   getWarning,
+  enProgramOversigt,
 };
